101eae77cc0a96fd64ddf917860ebc65d392fa86
i <iker.mataran@gmail.com>
2025-03-28 19:05:02 +0100

    Nueva clase Animal

---
+++ Animal.php
@@ -0,0 +1,11 @@
+<?php
+class Animal{
+    private $nom;
+    private $edat;
+
+    public function __construct($nomP, $edatP){
+        $this->nom = $nomP;
+        $this->edat = $edatP;
+    }
+}
+?>
